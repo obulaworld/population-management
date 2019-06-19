0539698f8d31d799556fe124d43b78d6554f80e1
obulaworld <obulaworldofexcellence@gmail.com>
2019-06-19 22:10:17 +0100

    ft(populationFeatures) build out the population features

--- src/controllers/PopulationController/index.js
+++ src/controllers/PopulationController/index.js
@@ -1,11 +1,36 @@
 import express from 'express';
 import PopulationController from './PopulationController';
+import PopulationValidator from '../../middlewares/PopulationValidator';
 
 const PopulationRouter = express.Router();
 
 PopulationRouter.get(
   '/',
   PopulationController.home,
+);
+
+PopulationRouter.get(
+  '/locations',
+  PopulationController.getLocations,
+);
+
+PopulationRouter.post(
+  '/locations',
+  PopulationValidator.checkFields,
+  PopulationController.createLocation,
+);
+
+PopulationRouter.put(
+  '/locations/:locationId',
+  PopulationValidator.checkParam,
+  PopulationValidator.checkFields,
+  PopulationController.updateLocation,
+);
+
+PopulationRouter.delete(
+  '/locations/:locationId',
+  PopulationValidator.checkParam,
+  PopulationController.deleteLocation,
 );
 
 export default PopulationRouter;
